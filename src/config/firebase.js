de930a74cee27fe8a64631401a6b4075bad0308e
saichandrakiranuppalapati45b4 <ukiranvarma@gmail.com>
2026-07-23 21:09:59 +0530

    Fix Firebase app export for Cloudflare deployment

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -13,10 +13,8 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-
-export default app;
